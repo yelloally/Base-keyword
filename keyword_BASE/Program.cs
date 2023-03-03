b2a67d54bb9186552b29c81bacde1f18f67423ce
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-03 13:30:13 +0100

    Add files via upload

---
+++ keyword_BASE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+
+    public virtual void Introduce()
+    {
+        Console.WriteLine("Hi, my name is " + Name);
+    }
+}
+
+class Student : Person
+{
+    public string Major { get; set; }
+
+    public override void Introduce()
+    {
+        base.Introduce();
+        Console.WriteLine("I am majoring in " + Major);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.Name = "Alona";
+        student.Major = "IT";
+        student.Introduce();
+
+        Console.ReadLine();
+    }
+}
